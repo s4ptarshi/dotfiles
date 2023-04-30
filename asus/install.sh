d2cd61a4a34192b02c02916399a0a25344d8ff1f
Saptarshi Bhattacharya <80621081+s4ptarshi@users.noreply.github.com>
2023-04-30 10:16:23 +0530

    fix script

--- asus/install.sh
+++ asus/install.sh
@@ -1,3 +1,3 @@
-sudo ln -sf $HOME/git-repos/dotfiles/asus/90-nkey.hwdb /etc/udev/hwdb.d/90-nkey.hwdb
+sudo ln -srf 90-nkey.hwdb /etc/udev/hwdb.d/90-nkey.hwdb
 sudo systemd-hwdb update
 sudo udevadm trigger
