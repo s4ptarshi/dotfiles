53f3c1f52f8d0015615b59915ec158b702202762
Saptarshi Bhattacharya <80621081+s4ptarshi@users.noreply.github.com>
2023-05-22 11:24:44 +0530

    update bar

---
+++ hyprland/.config/waybar/scripts/activewindow/active-window.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+while [[ true ]]; do
+    ACTIVE=$(hyprctl activewindow -j)
+    TITLE=$(echo $ACTIVE | jq -r '.title')
+
+    if [[ "$TITLE" == "null" ]] || [[ "$TITLE" == "" ]]; then
+        TEXT=" $(whoami)@$(hostname)"
+        TOOLTIP="$(uname -s) $(uname -r)"
+    else
+        TEXT="${TITLE}"
+        TOOLTIP="class: $(echo $ACTIVE | jq -r '.class')\nxwayland: $(echo $ACTIVE | jq -r '.xwayland')"
+    fi
+
+    JSON=$(echo "{\"text\":\"${TEXT}\", \"tooltip\":\"${TOOLTIP}\"}" | sed 's/&/\&amp;/g')
+
+    echo "${JSON}"
+done
