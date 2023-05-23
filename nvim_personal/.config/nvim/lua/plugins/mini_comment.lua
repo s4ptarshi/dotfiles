f5b319cbc38848833bffea4ac58ba3333eaf3aaf
Saptarshi Bhattacharya <80621081+s4ptarshi@users.noreply.github.com>
2023-05-23 17:27:54 +0530

    update config

---
+++ nvim_personal/.config/nvim/lua/plugins/mini_comment.lua
@@ -0,0 +1,13 @@
+return {
+    "echasnovski/mini.comment",
+    opts = {
+        mappings = {
+            -- Normal and Visual modes
+            comment = "<leader>/",
+            -- Toggle comment on current line
+            comment_line = "<leader>/",
+            -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+            textobject = "gc",
+        },
+    },
+}
