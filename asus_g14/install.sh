c378b4397641497264fdb797879924cc009dba8e
Saptarshi Bhattacharya <80621081+s4ptarshi@users.noreply.github.com>
2026-05-04 10:34:52 +0200

    update install script

--- asus_g14/install.sh
+++ asus_g14/install.sh
@@ -1,4 +1,5 @@
-sudo ln -srf 90-nkey.hwdb /etc/udev/hwdb.d/90-nkey.hwdb
+sudo sp 90-nkey.hwdb /etc/udev/hwdb.d/
 sudo systemd-hwdb update
 sudo udevadm trigger
+sudo reinstall-kernels && sudo dracut --force
 stow anime-matrix
