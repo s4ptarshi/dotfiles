9762c989acb426210a7a59ca17bc38b5e1c33bf6
Saptarshi Bhattacharya <80621081+s4ptarshi@users.noreply.github.com>
2026-05-28 12:30:31 +0200

    feat: add ssh-unlock and vert_mon_dark fish functions

---
+++ fish/.config/fish/functions/ssh-unlock.fish
@@ -0,0 +1,3 @@
+function ssh-unlock --wraps='eval (ssh-agent -c) && ssh-add ~/.ssh/id_ed25519' --description 'alias ssh-unlock=eval (ssh-agent -c) && ssh-add ~/.ssh/id_ed25519'
+    eval (ssh-agent -c) && ssh-add ~/.ssh/id_ed25519 $argv
+end
---
+++ fish/.config/fish/functions/vert_mon_dark.fish
@@ -0,0 +1,3 @@
+function vert_mon_dark --wraps='dms ipc call wallpaper setFor DP-1 ~/Pictures/goldy/goldy_portrait_dark.png' --description 'alias vert_mon_dark=dms ipc call wallpaper setFor DP-1 ~/Pictures/goldy/goldy_portrait_dark.png'
+    dms ipc call wallpaper setFor DP-1 ~/Pictures/goldy/goldy_portrait_dark.png $argv
+end
