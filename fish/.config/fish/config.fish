70133e4bb5af154962ec930bcf3135367aa49567
Saptarshi Bhattacharya <80621081+s4ptarshi@users.noreply.github.com>
2026-06-24 23:27:55 +0200

    refactor(fish): optimize conda lazy-loading and add agy bin to PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -100,22 +100,32 @@ if status is-interactive # Commands to run in interactive sessions can go here
     end
 end
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-#
-# Uses the first conda installation found in the following list
 set -x CONDA_PATH /data/miniconda3/bin/conda $HOME/miniconda3/bin/conda /netscratch/bhattach/miniconda3/bin/conda
 
-function conda
-    echo "Lazy loading conda upon first invocation..."
-    functions --erase conda
-    for conda_path in $CONDA_PATH
-        if test -f $conda_path
-            echo "Using Conda installation found in $conda_path"
-            eval $conda_path "shell.fish" hook | source
-            conda $argv
-            return
+# 1. VSCodium/VSCode behavior: Silent, instant initialization
+if set -q TERM_PROGRAM; and string match -q -r '(vscode|VSCodium)' "$TERM_PROGRAM"
+    for c_path in $CONDA_PATH
+        if test -f $c_path
+            eval $c_path "shell.fish" hook | source
+            break
         end
     end
-    echo "No conda installation found in $CONDA_PATH"
+    # 2. Normal terminal behavior: Lazy loading for fast startup
+else
+    function conda
+        functions --erase conda
+        for c_path in $CONDA_PATH
+            if test -f $c_path
+                echo "Lazy loading conda..."
+                eval $c_path "shell.fish" hook | source
+                conda $argv
+                return
+            end
+        end
+        echo "No conda installation found."
+    end
 end
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/home/s4ptarshi/.local/bin" $PATH
